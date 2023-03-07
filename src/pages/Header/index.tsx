594af18ec9fd9480f1257e68f09980dc8fec99bb
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-03-07 20:33:27 -0300

    up project

---
+++ src/pages/Header/index.tsx
@@ -0,0 +1,18 @@
+export default function Header() {
+    return(
+        <>
+            <header className="w-auto ml-12 mr-12 mt-6">
+                <div className="flex justify-between">
+                    <div className="flex gap-8">
+                        <span>Skills</span>
+                        <span>Experience</span>
+                        <span>Projects</span>
+                    </div>
+                    <div>
+                        icons
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'background': '#030304',
+      'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#7c7c7c',
+      'gray': '#0A0A14',
+      'gray-light': '#d3dce6',
+      'linkedin': '#0e76a8',
+      'mail': '#833AB4',
+      'github': '#171515'
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      backgroundImage: {
+        'header': "url('/Header-card.svg')"
+      }
+    }
+  },
+  plugins: [],
+}
