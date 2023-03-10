c6c2f9acb334e660f7aeef6d92e9377dd302fdab
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-03-09 21:39:45 -0300

    feat: add banner in site

---
+++ src/pages/Banner/index.tsx
@@ -0,0 +1,17 @@
+export default function Apresentation() {
+    return(
+        <>
+            <div className="flex pt-36 items-center justify-center gap-36">
+                <div className="">
+                    <h1 className="text-3xl">Hello there, welcome to my site</h1>
+                    <p className="text-6xl text-linkedin">I´m Alex Bruno</p>
+                    <p className="text-6xl">A Full Stack Developer</p>
+                    
+                </div>
+              
+                <img src="/public/Avatar.svg" alt="" className="w-96 h-96 border-solid border-[1.25rem] rounded-4xl border-[bg-gradient-to-r from-cyan-500 to-blue-500]" />
+          
+            </div>
+        </>
+    )
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import Apresentation from "./Banner";
 import Header from "./Header";
+import Skills from "./Skills";
 
 export default function Home() {
     return(
-        <>
-        <Header />
-        </>
+        <div className="w-auto ml-[8.625rem] mr-[8.625rem] mt-6">
+            <Header />
+            <Apresentation />
+            <Skills/>
+        </div>
     )
 };
