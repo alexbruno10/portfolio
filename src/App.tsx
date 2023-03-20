6072df2130a55e2aaffd09db321ff66899aa4836
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-03-20 19:12:12 -0300

    feat: add React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import '../src/styles/global.scss'
 import Header from './components/Header'
 import Home from './pages/Home'
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   )
 }
 
