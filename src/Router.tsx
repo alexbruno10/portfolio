79ecd5e980ccead392a9d4d8dea73c33df27bdb3
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-03-20 21:09:24 -0300

    feat: add page  'About'

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layout/DefaultLayout";
 import Home from "./pages/Home";
+import About from "./pages/Sobre";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
       <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<About />} />
       </Route>
     </Routes>
   );
