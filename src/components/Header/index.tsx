1bac6da21ab3b0f8d473fcb49e6fe15182a9a7a4
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-03-20 21:09:52 -0300

    style: change style in links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,50 @@
 import {Github, Linkedin, Mail } from 'lucide-react'
 import style from './styles.module.scss'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+interface ActiveLink {
+    class: string,
+}
+
 
 export default function Header() {
+
+    const [activeLink, setActiveLink] = useState<ActiveLink>({class: 'home'});
+
+    function handleActiveLink (name: string) {
+        console.log(name)
+        setActiveLink(name);
+        console.log(activeLink)
+    }
+
     return(
         <>
             <header className={style.header}>
-                <h1>AB</h1>
+                <Link to={`/`}>
+                    <h1>AB</h1>
+                </Link>
                 <div className={style.sections}>
-                    <a href="">Home</a>
-                    <a href="">Sobre</a>
-                    <a href="">Skills</a>
-                    <a href="">Projetos</a>
+                    <Link to={`/`}>
+                    Home
+                    </Link>
+                    <Link className={style.active} to={`/sobre`} onClick={() => handleActiveLink('sobre')}>
+                    Sobre
+                    </Link>
+                    <Link to={`/`}>
+                    Projetos
+                    </Link>
                 </div>
-                <div className={style.sections}>
-                    <Github color='var(--white)' size={20}/>
-                    <Linkedin color='var(--white)' size={20} />
-                    <Mail color='var(--white)' size={20}/>
+                <div className={style.icons}>
+                    <Link to={`https://github.com/alexbruno10`} target="_blank">
+                        <span className={style.github}><Github size={20}/></span>
+                    </Link>
+                    <Link to={`https://www.linkedin.com/in/alexbruno10/`}>
+                        <span className={style.linkedin}><Linkedin size={20} target="_blank"/></span>
+                    </Link>
+                    <Link to={`mailto:alexbruno1298@gmail.com`}>
+                        <span className={style.mail}><Mail size={20} target="_blank"/></span>
+                    </Link>  
                 </div>
             </header>
         </>
