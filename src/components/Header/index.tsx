2d11873a3ddb1379c90ea352debbacf33261f556
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-03-22 19:47:10 -0300

    fix: adjust open link in icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -33,11 +33,11 @@ export default function Header() {
                     <Link to={`https://github.com/alexbruno10`} target="_blank">
                         <span className={style.github}><Github size={20}/></span>
                     </Link>
-                    <Link to={`https://www.linkedin.com/in/alexbruno10/`}>
-                        <span className={style.linkedin}><Linkedin size={20} target="_blank"/></span>
+                    <Link to={`https://www.linkedin.com/in/alexbruno10/`} target="_blank">
+                        <span className={style.linkedin}><Linkedin size={20} /></span>
                     </Link>
-                    <Link to={`mailto:alexbruno1298@gmail.com`}>
-                        <span className={style.mail}><Mail size={20} target="_blank"/></span>
+                    <Link to={`mailto:alexbruno1298@gmail.com`} target="_blank">
+                        <span className={style.mail}><Mail size={20} /></span>
                     </Link>  
                 </div>
             </header>
