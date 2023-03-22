9b78b2386f69d7d69d0bf05fd30c5e7a606aa41f
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-03-21 21:01:38 -0300

    feat: create animation in svg

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import styles from './styles.module.scss'
+import { ChevronsRight } from 'lucide-react'
 
 export default function Home() {
     return(
         <div className={styles.content}>
             <div>
                 <span>Hello there, welcome to my site</span>
-                <h1>I´m Alex Bruno</h1>
-                <h1>A Full Stack Developer</h1>
+                <h1>I´m <span className={styles.name}>Alex Bruno Neri Humberto</span></h1>
+                <h2>Full Stack Developer</h2>
+                <span className={styles.aboutMe}>About me <ChevronsRight /></span>
             </div>
         </div>
     )
