aa63a808f76e3737e0565d248c05cb507a1f611b
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-03-28 21:32:45 -0300

    feat: change content

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -15,25 +15,44 @@ export default function About() {
             <section className={styles.content}>
                 <section className={styles.contentDescription}>
                     <div className={styles.contentTitle}>
-                        <h1>About me</h1>
+                        <h1>Sobre mim</h1>
                     </div>
                     <div className={styles.contentAbout}>
                         <div className={styles.contentParagraph}>
-                            <p>🤙🏻 Hello! My name is <span>Alex Bruno Neri Humberto</span>, i´m 24 years old!</p>
-                            <p>🎓 Graduated in <span>Computer Science</span>.</p>
-                            <p>💻 <span>Full Stack Developer</span> since 2020.</p>
+                            <p>🤙🏻 Me chamo <span>Alex Bruno Neri Humberto</span>, tenho 24 anos!</p>
+                            <p>🎓 Graduado em <span>Ciência da Computação</span>.</p>
+                            <p>💻 <span>Desenvolvedor Full Stack</span> com experiência em HTML, CSS, SASS, Styled-Components, JavaScript, TypeScript, React JS, PHP, Laravel e Banco de Dados SQL e NoSQL.</p>
+                            <p>🚀 <span>Desenvolvedor Full Stack</span></p>
                         </div>
                         <img src="https://avatars.githubusercontent.com/u/57117620?v=4" alt="" />
                     </div>
                 </section>
-                <section className={styles.contentSkills}>
-                    <div className={styles.contentTitle}>
-                        <h1>Skills</h1>
+                <section className={styles.carrer}>
+                    <div className={styles.carrerTitle}>
+                        <h1>Carreira</h1>
                     </div>
-                    <div className={styles.contentIcons}>
+                    <div className={styles.contentCarrer}>
+                        <div className={styles.contentCarrerDetails}>
+                            <span><strong>Desenvolvedor Full Stack</strong></span>
+                            <p>Sicoob Credivar - Analista de T.I </p>
+                            <p>Novembro 2020 - Presente</p>
+                        </div>
+                        <div className={styles.contentCarrerDetails}>
+                            <span>Estágio</span>
+                            <p>Sicoob Credivar</p>
+                            <p>Março 2019 - Novembro 2020</p>
+                        </div>
+                        <div className={styles.contentCarrerDetails}>
+                            <span>Auxiliar Administrativo</span>
+                            <p>Pedras Decorativas Líder</p>
+                            <p>Agosto 2017 - Fevereiro 2019</p>
+                        </div>
+                    </div>
+                    
+
+                    {/* Terminal com skills */}
+                    {/* <div className={styles.contentIcons}>
                         <div className={styles.contentTerminal}>
-                            
-                            
                             <code>
                             <div className={styles.contentTerminalHeader}>
                                 <div className={styles.contentTerminalButtons}>
@@ -44,12 +63,12 @@ export default function About() {
                                 <p>alexbruno.dev</p>
                             </div>
                             <SyntaxHighlighter language="javascript" style={atomDark}>
-                            {JSON.stringify(Skills, null, 2) }
+                            {JSON.stringify(Skills, null, 1) }
                             </SyntaxHighlighter>
                             </code>
                             
                         </div>
-                    </div>
+                    </div> */}
                 </section>
             </section>
         </>
