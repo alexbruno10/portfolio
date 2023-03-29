834e8aa6ecf9b7e34f4baec059fadaf21a629e85
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-03-28 21:38:30 -0300

    feat: add new animation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,11 +6,11 @@ export default function Home() {
     return(
         <div className={styles.content}>
             <div>
-                <span>Hello there, welcome to my site</span>
-                <h1>I´m <span className={styles.name}>Alex Bruno Neri Humberto</span></h1>
-                <h2>Full Stack Developer</h2>
+                <span>Olá, bem-vindo ao meu site!</span>
+                <h1>Eu sou <span className={styles.name}>Alex Bruno Neri Humberto</span></h1>
+                <h2>Desenvolvedor Full Stack</h2>
                 <Link to={`/about`}>
-                    <span className={styles.aboutMe}>About me <ChevronsRight /></span>
+                    <span className={styles.aboutMe}>Sobre mim <ChevronsRight /></span>
                 </Link>
             </div>
         </div>
